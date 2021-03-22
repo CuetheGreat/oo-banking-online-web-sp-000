8736c4604fa11e3bd087016abb56afd08ce34a94
Clayon Stewart <c.stewart89@gmail.com>
2021-03-22 18:36:40 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,29 @@
 class BankAccount
+  attr_accessor :balance, :status
 
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+  
+  def deposit(amount)
+    @deposit += amount
+  end
+  
+  def display_balance
+    @deposit
+  end
+  
+  def valid?
+    if @status == "open" && @balance > 0
+      return true
+    else
+      return false
+    end
+  end
+  
+  def close_account
+    @status = "closed"
+  end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,29 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status, :transfer_amount
+  
+  def initialize(sender,receiver, transfer_amount)
+    @sender= sender
+    @receiver = receiver
+    @status = "pending"
+    @transfer_amount = transfer_amount
+  end
+  
+  def valid?
+    if @sender.valid? && @receiver.valid?
+      return true
+    else
+      return false
+    end
+  end
+  
+  def execute_transaction
+    if valid?
+      @receiver.deposit(@transfer_amount)
+    else
+      return false
+    end
+  end
+  
+  
+    
 end
