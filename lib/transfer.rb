17d2b4351d256b651b5fa41f65c69d890877da90
Clayon Stewart <c.stewart89@gmail.com>
2021-03-22 19:04:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -27,7 +27,7 @@ class Transfer
     end
   end
   
-  def reverse_transaction
+  def reverse_transfer
     if status == "complete"
       @sender.deposit(@amount)
       @receiver.withdraw(@amount)
