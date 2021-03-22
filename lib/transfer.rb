d6890c04b7210425183f00525058a8e7211fc1fa
Clayon Stewart <c.stewart89@gmail.com>
2021-03-22 18:53:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -7,6 +7,7 @@ class Transfer
     @status = "pending"
     @amount = transfer_amount
     puts "amount: #{@amount}, sender: #{@sender.balance}, receiver: #{@receiver.balance} "
+    @count = 1
   end
   
   def valid?
@@ -18,10 +19,11 @@ class Transfer
   end
   
   def execute_transaction
-    if valid?
+    if valid? && @count > 0
       @receiver.deposit(@amount)
       @sender.withdraw(@amount)
       @status = "complete"
+      @count = 0
     else
       return false
     end
